adaba46b46e4c12069f09dc90441462a49475c80
Pouya Barari.G <b_pooya@yahoo.com>
2022-10-20 15:02:53 +0330

    hh

--- src/SignUpForm/index.js
+++ src/SignUpForm/index.js
@@ -1,26 +1,59 @@
+import { useState } from 'react';
+import { useFormik } from 'formik';
 const SignUpForm = () => {
-  return (<>
-    <div>
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const changeHandler = ({ target }) => {
+    setUserData({ ...userData, [target.name]: target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
+  return (
+    <>
       <div>
-        <p className="main-p">Welcome to PBG form</p>
+        <div>
+          <p className='main-p'>Welcome to PBG form</p>
+        </div>
+        <form onSubmit={submitHandler}>
+          <div className='formControl'>
+            <label>Name</label>
+            <input
+              type='text'
+              onChange={changeHandler}
+              value={userData.name}
+              name='name'
+            />
+          </div>
+          <div className='formControl'>
+            <label>Email</label>
+            <input
+              type='text'
+              onChange={changeHandler}
+              value={userData.email}
+              name='email'
+            />
+          </div>
+          <div className='formControl'>
+            <label>Password</label>
+            <input
+              type='text'
+              onChange={changeHandler}
+              value={userData.password}
+              name='password'
+            />
+          </div>
+          <button>sumbit</button>
+        </form>
       </div>
-      <form>
-        <div className="formControl">
-          <label>Name</label>
-          <input type="text" />
-        </div>
-        <div className="formControl">
-          <label>Email</label>
-          <input type="text" />
-        </div>
-        <div className="formControl">
-          <label>Password</label>
-          <input type="text" />
-        </div>
-        <button>sumbit</button>
-      </form>
-    </div>
-  </>);
-}
+    </>
+  );
+};
 
 export default SignUpForm;
