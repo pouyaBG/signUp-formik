eda6556fb6e2954e41387b4746de687520ca9971
Pouya Barari.G <b_pooya@yahoo.com>
2022-10-20 12:29:28 +0330

    c

--- src/SignUpForm/index.js
+++ src/SignUpForm/index.js
@@ -1,5 +1,25 @@
 const SignUpForm = () => {
-  return (<>dd
+  return (<>
+    <div>
+      <div>
+        <p className="main-p">Welcome to PBG form</p>
+      </div>
+      <form>
+        <div className="formControl">
+          <label>Name</label>
+          <input type="text" />
+        </div>
+        <div className="formControl">
+          <label>Email</label>
+          <input type="text" />
+        </div>
+        <div className="formControl">
+          <label>Password</label>
+          <input type="text" />
+        </div>
+        <button>sumbit</button>
+      </form>
+    </div>
   </>);
 }
 
