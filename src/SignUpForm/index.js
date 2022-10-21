f1688a7f41f997d5497ce3277ef584db25597396
Pouya Barari.G <b_pooya@yahoo.com>
2022-10-21 20:11:42 +0330

    formik

--- src/SignUpForm/index.js
+++ src/SignUpForm/index.js
@@ -1,55 +1,89 @@
 import { useState } from 'react';
 import { useFormik } from 'formik';
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+}
+
+// error validation
+const validate = (values) => {
+  let errors = {};
+  if (!values.name) { errors.name = "Please enter a name!" }
+  if (!values.email) { errors.email = "Please enter a email!" }
+  if (!values.password) { errors.password = "Please enter a password!" }
+  return errors
+}
+
+
 const SignUpForm = () => {
-  const [userData, setUserData] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
 
-  const changeHandler = ({ target }) => {
-    setUserData({ ...userData, [target.name]: target.value });
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log(e);
+  const onSubmit = (values) => {
+    console.log(values);
   }
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit: onSubmit,
+    validate: validate
+  })
+  console.table(formik.touched);
+  // const [userData, setUserData] = useState({
+  //   name: '',
+  //   email: '',
+  //   password: '',
+  // });
+
+  // const changeHandler = ({ target }) => {
+  //   setUserData({ ...userData, [target.name]: target.value });
+  // };
+
+  // const submitHandler = (e) => {
+  //   e.preventDefault();
+  //   console.log(e);
+  // }
   return (
     <>
       <div>
         <div>
           <p className='main-p'>Welcome to PBG form</p>
         </div>
-        <form onSubmit={submitHandler}>
+        <form onSubmit={formik.handleSubmit}>
           <div className='formControl'>
             <label>Name</label>
             <input
               type='text'
-              onChange={changeHandler}
-              value={userData.name}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.name}
               name='name'
             />
+            {formik.errors.name && formik.touched.name && <div className='error'>{formik.errors.name}</div>}
           </div>
           <div className='formControl'>
             <label>Email</label>
             <input
               type='text'
-              onChange={changeHandler}
-              value={userData.email}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.email}
               name='email'
             />
+            {formik.errors.email && formik.touched.email && <div className='error'>{formik.errors.email}</div>}
           </div>
           <div className='formControl'>
             <label>Password</label>
             <input
               type='text'
-              onChange={changeHandler}
-              value={userData.password}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.password}
               name='password'
             />
+            {formik.errors.password && formik.touched.password && <div className='error'>{formik.errors.password}</div>}
           </div>
-          <button>sumbit</button>
+          <button type='sumbit'>sumbit</button>
         </form>
       </div>
     </>
